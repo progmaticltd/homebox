c9a5af9dca492d9393404dcc12cedcdd13ffb775
André Rodier <andre@rodier.me>
2018-03-20 22:35:40 +0000

    Started to implement preseed with encrypted LUKS and softwar RAID

--- preseed/playbooks/roles/build-iso/templates/build.sh
+++ preseed/playbooks/roles/build-iso/templates/build.sh
@@ -3,47 +3,46 @@
 # The first parameter is the name of the server to build the iso image
 HOSTNAME=$1
 
+{% if debug %}
+set -x
+{% endif %}
+
+# Remove the previous version
+rm -f '{{ build_dir }}/images/debian-{{ system.version }}-{{ system.arch }}-DVD-1.iso'
+
 # Use proxy if defined
-{% if network.main.proxy %}
-export http_proxy='{{ network.main.proxy }}'
+{% if network.proxy is defined %}
+export http_proxy='{{ network.proxy }}'
 {% endif %}
 
 # Parameters
 DIST='{{ repo.release }}'
 LOCALE='{{ locale.id }}'
 
-# Extra profiles to include
-PROFILES='{{ cdimage.profiles }}'
-
 # Mirror (TODO: Check httpredir ?)
 MIRROR='http://{{ repo.main }}/debian/'
 
 # Common options
-COMMON_OPTS="--force-root --debian-mirror ${MIRROR} --locale ${LOCALE} --dist ${DIST}"
+COMMON_OPTS="--debian-mirror ${MIRROR} --locale ${LOCALE} --dist ${DIST} --debug"
 
 # Build the mirror repositories
 OPTIONS="--do-mirror ${COMMON_OPTS}"
 simple-cdd $OPTIONS
 
 # Create the miscellaneous files archive
-tar c --remove-files -C {{ build_dir }}/misc -z -f {{ build_dir }}/misc.tgz .
+tar c -C "{{ playbook_dir }}/../misc" -z -f "{{ playbook_dir }}/../build/misc.tgz" .
 
 # Build installer CDs for the whole platform
 OPTIONS="--verbose"
 OPTIONS+=" --build-only ${COMMON_OPTS}"
 OPTIONS+=" --dvd --conf common.conf"
 
-# Add extra profiles if defined
-if [ "$PROFILES" ne "" ]; then
-    OPTIONS+="--profiles $PROFILES "
-fi
-
 # Set keyboard configuration
 OPTIONS+=" --keyboard {{ locale.keymap }}"
 
 # Where to save the logs:
 LOGFILE="{{ build_dir }}/logs/${HOSTNAME}-cdd.log"
-test -d {{ build_dir }}/logs || mkdir {{ build_dir }}/logs
+test -d "{{ build_dir }}/logs" || mkdir "{{ build_dir }}/logs"
 
 OPTIONS+=" --logfile ${LOGFILE}"
 
