feea500c2b994243b4a95aa37a2fe3faf507d191
André Rodier <andre@rodier.me>
2018-03-30 08:13:15 +0100

    Started to work on impersonate

---
+++ install/playbooks/roles/roundcube/templates/dovecot_impersonate.php
@@ -0,0 +1,6 @@
+<?php
+
+// set the dovecot seperator character. Check your dovecot config file for 'auth_master_user_separator'
+// to read about this dovecot feature go to:  http://wiki.dovecot.org/Authentication/MasterUsers
+
+$rcmail_config['dovecot_impersonate_seperator'] = '/';
