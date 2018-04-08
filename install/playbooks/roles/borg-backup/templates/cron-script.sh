2163b078fb9068e42b6c084b328cf133664fa65b
André Rodier <andre@rodier.me>
2018-04-08 08:57:07 +0100

    Updates on backup
    Do not start a backup process if the same is already running
    Fixed compression flag default value
    Updated documentation

--- install/playbooks/roles/borg-backup/templates/cron-script.sh
+++ install/playbooks/roles/borg-backup/templates/cron-script.sh
@@ -1,4 +1,8 @@
 #!/bin/dash
 
 # Call the global script helper
-/usr/local/sbin/backup '{{ location.name }}'
+{% if system.debug %}
+/usr/local/sbin/backup --config '{{ location.name }}' --log-level DEBUG
+{% else %}
+/usr/local/sbin/backup --config '{{ location.name }}'
+{% endif %}
