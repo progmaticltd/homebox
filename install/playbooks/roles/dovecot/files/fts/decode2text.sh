4256ce61ce4194051661dbcadd658021b48563a8
André Rodier <andre@rodier.me>
2018-04-28 22:22:33 +0100

    Attachment indexing updates
    Use linux tools for sxw, docx and xlsx.
    Improved AppArmor profiles.

--- install/playbooks/roles/dovecot/files/fts/decode2text.sh
+++ install/playbooks/roles/dovecot/files/fts/decode2text.sh
@@ -1,21 +1,12 @@
-#!/bin/dash
+#!/bin/bash
 
 # Example attachment decoder script. The attachment comes from stdin, and
 # the script is expected to output UTF-8 data to stdout. (If the output isn't
 # UTF-8, everything except valid UTF-8 sequences are dropped from it.)
 
-# The attachment decoding is enabled by setting:
-#
-# plugin {
-#   fts_decoder = decode2text
-# }
-# service decode2text {
-#   executable = script /usr/local/libexec/dovecot/decode2text.sh
-#   user = dovecot
-#   unix_listener decode2text {
-#     mode = 0666
-#   }
-# }
+# Add some logs
+# date=$(date -Iseconds)
+# echo "${date}: $1" >>/var/log/decode2txt.log
 
 libexec_dir=`dirname $0`
 content_type=$1
@@ -35,73 +26,68 @@ application/vnd.openxmlformats-officedocument.wordprocessingml.document docx
 application/vnd.openxmlformats-officedocument.spreadsheetml.sheet xlsx
 application/vnd.openxmlformats-officedocument.presentationml.presentation pptx
 application/vnd.oasis.opendocument.text odt
+application/vnd.sun.xml.writer sxw
 application/vnd.oasis.opendocument.spreadsheet ods
 application/vnd.oasis.opendocument.presentation odp
 '
 
 if [ "$content_type" = "" ]; then
-  echo "$formats"
-  exit 0
+    echo "$formats"
+    exit 0
 fi
 
 fmt=`echo "$formats" | grep -w "^$content_type" | cut -d ' ' -f 2`
 if [ "$fmt" = "" ]; then
-  echo "Content-Type: $content_type not supported" >&2
-  exit 1
+    echo "Content-Type: $content_type not supported" >&2
+    exit 1
 fi
 
-# most decoders can't handle stdin directly, so write the attachment
-# to a temp file
-path=`mktemp`
+# most decoders can't handle stdin directly, so write the attachment to a temp file
+path=$(mktemp)
 trap "rm -f $path" 0 1 2 3 14 15
 cat > $path
 
-xmlunzip() {
-  name=$1
-
-  tempdir=`mktemp -d`
-  if [ "$tempdir" = "" ]; then
-    exit 1
-  fi
-  trap "rm -rf $path $tempdir" 0 1 2 3 14 15
-  cd $tempdir || exit 1
-  unzip -q "$path" 2>/dev/null || exit 0
-  find . -name "$name" -print0 | xargs -0 cat |
-    $libexec_dir/xml2text
-}
-
 wait_timeout() {
-  childpid=$!
-  trap "kill -9 $childpid; rm -f $path" 1 2 3 14 15
-  wait $childpid
+    childpid=$!
+    trap "kill -9 $childpid; rm -f $path" 1 2 3 14 15
+    wait $childpid
 }
 
-
+# TODO: Check if this can be get from the system
 LANG=en_US.UTF-8
 export LANG
 
 if [ $fmt = "pdf" ]; then
-  /usr/bin/pdftotext $path - 2>/dev/null&
-  wait_timeout 2>/dev/null
+    /usr/bin/pdftotext $path - 2>/dev/null &
+    wait_timeout 2>/dev/null
 elif [ $fmt = "doc" ]; then
-  (/usr/bin/catdoc $path; true) 2>/dev/null&
-  wait_timeout 2>/dev/null
+    (/usr/bin/catdoc $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
 elif [ $fmt = "ppt" ]; then
-  (/usr/bin/catppt $path; true) 2>/dev/null&
-  wait_timeout 2>/dev/null
+    (/usr/bin/catppt $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
 elif [ $fmt = "xls" ]; then
-  (/usr/bin/xls2csv $path; true) 2>/dev/null&
-  wait_timeout 2>/dev/null
-elif [ $fmt = "odt" -o $fmt = "ods" -o $fmt = "odp" ]; then
-  xmlunzip "content.xml"
+    (/usr/bin/xls2csv $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
+elif [ $fmt = "odt" ]; then
+    (/usr/bin/odt2txt $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
+elif [ $fmt = "sxw" ]; then
+    (/usr/bin/sxw2txt $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
+elif [ $fmt = "ods" ]; then
+    (/usr/bin/ods2txt $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
 elif [ $fmt = "docx" ]; then
-  xmlunzip "document.xml"
+    (/usr/bin/docx2txt $path -; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
 elif [ $fmt = "xlsx" ]; then
-  xmlunzip "sharedStrings.xml"
-elif [ $fmt = "pptx" ]; then
-  xmlunzip "slide*.xml"
+    (/usr/bin/xlsx2csv $path; true) 2>/dev/null &
+    wait_timeout 2>/dev/null
 else
-  echo "Buggy decoder script: $fmt not handled" >&2
-  exit 1
+    echo "Buggy decoder script: $fmt not handled" >&2
+    exit 1
 fi
+
+
 exit 0
