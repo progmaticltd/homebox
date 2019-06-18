8687ac5c02f7023322ec35ea099c3ae105deaacb
André Rodier <andre@rodier.me>
2019-06-18 13:52:59 +0100

    Use a custom name for the backup and restore script
    This should avoid clashes with other binaries called ‘backup’ on Debian.

--- install/playbooks/roles/borg-backup/templates/cron-backup-script.sh
+++ install/playbooks/roles/borg-backup/templates/cron-backup-script.sh
@@ -2,7 +2,7 @@
 
 # Call the global script helper
 {% if system.debug %}
-/usr/local/sbin/backup --config '{{ location.name }}' --log-level DEBUG
+/usr/local/sbin/homebox-backup --config '{{ location.name }}' --log-level DEBUG
 {% else %}
-/usr/local/sbin/backup --config '{{ location.name }}'
+/usr/local/sbin/homebox-backup --config '{{ location.name }}'
 {% endif %}
