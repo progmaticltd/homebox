e0a5fd4e3c811c8f4ae2914c0c1bdcd0ca3f2d13
André Rodier <andre@rodier.me>
2019-08-04 21:10:00 +0100

    First python implementation, win an SQLite database

--- install/playbooks/roles/sogo-milters/files/milter-abook.py
+++ install/playbooks/roles/sogo-milters/files/milter-abook.py
@@ -14,6 +14,8 @@ import StringIO
 import time
 import email
 import sys
+import sqlite3
+
 from socket import AF_INET, AF_INET6
 from Milter.utils import parse_addr
 
@@ -25,19 +27,24 @@ else:
 
 logq = Queue(maxsize=4)
 
-class abookMilter(Milter.Base):
+class markAddressBookMilter(Milter.Base):
 
-    def __init__(self):  # A new instance with each new connection.
-        self.id = Milter.uniqueID()  # Integer incremented with each call.
+    # A new instance with each new connection.
+    def __init__(self):
+        # Integer incremented with each call.
+        self.id = Milter.uniqueID()
 
-    # each connection runs in its own thread and has its own abookMilter
-    # instance.  Python code must be thread safe.  This is trivial if only stuff
-    # in abookMilter instances is referenced.
+    # Proper exit
+    # def __exit__(self, exc_type, exc_value, traceback):
+
+    # each connection runs in its own thread and has its own markAddressBookMilter
+    # instance. Python code must be thread safe.    This is trivial if only stuff
+    # in markAddressBookMilter instances is referenced.
     @Milter.noreply
     def connect(self, IPname, family, hostaddr):
         # (self, 'ip068.subnet71.example.com', AF_INET, ('215.183.71.68', 4720) )
         # (self, 'ip6.mxout.example.com', AF_INET6,
-        #   ('3ffe:80e8:d8::1', 4720, 1, 0) )
+        #     ('3ffe:80e8:d8::1', 4720, 1, 0) )
         self.IP = hostaddr[0]
         self.port = hostaddr[1]
         if family == AF_INET6:
@@ -46,59 +53,57 @@ class abookMilter(Milter.Base):
         else:
             self.flow = None
             self.scope = None
-        self.IPname = IPname  # Name from a reverse IP lookup
-        self.H = None
-        self.fp = None
-        self.receiver = self.getsymval('j')
-        self.log("connect from %s at %s" % (IPname, hostaddr) )
+            self.IPname = IPname    # Name from a reverse IP lookup
+            self.H = None
+            self.fp = None
+            self.receiver = self.getsymval('j')
+            self.log("connect from %s at %s" % (IPname, hostaddr))
 
         return Milter.CONTINUE
 
 
-    ##  def hello(self,hostname):
+    ## def hello(self,hostname):
     def hello(self, heloname):
-        # (self, 'mailout17.dallas.texas.example.com')
         self.H = heloname
         self.log("HELO %s" % heloname)
-        if heloname.find('.') < 0:  # illegal helo name
+        if heloname.find('.') < 0: # illegal helo name
             # NOTE: example only - too many real braindead clients to reject on this
-            self.setreply('550','5.7.1','Sheesh people!  Use a proper helo name!')
+            self.setreply('550','5.7.1','Sheesh people! Use a proper helo name!')
             return Milter.REJECT
 
         return Milter.CONTINUE
 
-    ##  def envfrom(self,f,*str):
+    ## def envfrom(self,f,*str):
     def envfrom(self, mailfrom, *str):
-        self.F = mailfrom
-        self.R = []  # list of recipients
-        self.fromparms = Milter.dictfromlist(str)   # ESMTP parms
+        self.mailFrom = '@'.join(parse_addr(mailfrom))
+        self.recipients = [] # list of recipients
+        self.fromparms = Milter.dictfromlist(str) # ESMTP parms
         self.user = self.getsymval('{auth_authen}') # authenticated user
         self.log("mail from:", mailfrom, *str)
-        # NOTE: self.fp is only an *internal* copy of message data.  You
+        self.log("user:", self.user, *str)
+        # NOTE: self.fp is only an *internal* copy of message data.    You
         # must use addheader, chgheader, replacebody to change the message
         # on the MTA.
         self.fp = StringIO.StringIO()
-        self.canon_from = '@'.join(parse_addr(mailfrom))
-        self.fp.write('From %s %s\n' % (self.canon_from,time.ctime()))
-
+        # self.canon_from = '@'.join(parse_addr(mailfrom))
+        # self.fp.write('From %s %s\n' % (self.canon_from,time.ctime()))
         return Milter.CONTINUE
 
-    ##  def envrcpt(self, to, *str):
+    ## def envrcpt(self, to, *str):
     @Milter.noreply
     def envrcpt(self, to, *str):
         rcptinfo = to,Milter.dictfromlist(str)
-        self.R.append(rcptinfo)
-
+        self.recipients.append(rcptinfo)
         return Milter.CONTINUE
 
     @Milter.noreply
     def header(self, name, hval):
-        self.fp.write("%s: %s\n" % (name,hval))     # add header to buffer
+        self.fp.write("%s: %s\n" % (name,hval)) # add header to buffer
         return Milter.CONTINUE
 
     @Milter.noreply
     def eoh(self):
-        self.fp.write("\n")                         # terminate headers
+        self.fp.write("\n") # terminate headers
         return Milter.CONTINUE
 
     @Milter.noreply
@@ -106,16 +111,39 @@ class abookMilter(Milter.Base):
         self.fp.write(chunk)
         return Milter.CONTINUE
 
+    # many milter functions can only be called from eom()
     def eom(self):
         self.fp.seek(0)
         msg = email.message_from_file(self.fp)
-        # many milter functions can only be called from eom()
-        # example of adding a Bcc:
-        self.addrcpt('<%s>' % 'spy@example.com')
+
+        # This implementation use a simple sqlite database
+        db = sqlite3.connect("/var/lib/milter-abook/addresses.db")
+
+        # Check if the email address is in the user's address book
+        cursor = db.cursor()
+        sources = []
+        for rec in self.recipients:
+            parts = parse_addr(rec[0])
+            email_recipient = "@".join(parts)
+            uid = parts[0]
+            query = 'select source,abook from addresses where uid="{0}" and email="{1}"'
+            print query.format(uid, self.mailFrom)
+            cursor.execute(query.format(uid, self.mailFrom))
+            for row in cursor:
+                sources.append('{0}:{1}'.format(row[0], row[1]))
+
+        if len(sources) > 0:
+            self.addheader("X-AddressBook", ','.join(sources))
+        else:
+            self.addheader("X-AddressBook", 'unknown')
+
+        # Cleanup
+        db.close()
+
         return Milter.ACCEPT
 
     def close(self):
-        # always called, even when abort is called.  Clean up
+        # always called, even when abort is called. Clean up
         # any external resources here.
         return Milter.CONTINUE
 
@@ -123,42 +151,38 @@ class abookMilter(Milter.Base):
         # client disconnected prematurely
         return Milter.CONTINUE
 
-    ## === Support Functions ===
-
     def log(self,*msg):
         logq.put((msg,self.id,time.time()))
 
 def background():
     while True:
         t = logq.get()
-        if not t:
-            break
+        if not t: break
         msg,id,ts = t
-        print("%s [%d]".format(time.strftime('%Y%b%d %H:%M:%S',time.localtime(ts)),id))
+        print "%s [%d]" % (time.strftime('%Y%b%d %H:%M:%S', time.localtime(ts)),id),
         # 2005Oct13 02:34:11 [1] msg1 msg2 msg3 ...
-        for i in msg:
-            print(i),
-        print()
+        for i in msg: print i,
+        print
 
 ## ===
 
 def main():
     bt = Thread(target=background)
     bt.start()
-    socketname = "/run/milter-abook.sock"
+    socketname = "/run/milter-abook/milter-abook.socket"
     timeout = 600
     # Register to have the Milter factory create instances of your class:
-    Milter.factory = abookMilter
+    Milter.factory = markAddressBookMilter
     flags = Milter.CHGBODY + Milter.CHGHDRS + Milter.ADDHDRS
     flags += Milter.ADDRCPT
     flags += Milter.DELRCPT
-    Milter.set_flags(flags)       # tell Sendmail which features we use
-    print("%s milter startup" % time.strftime('%Y%b%d %H:%M:%S'))
+    Milter.set_flags(flags) # tell Sendmail which features we use
+    print "%s milter startup" % time.strftime('%Y%b%d %H:%M:%S')
     sys.stdout.flush()
     Milter.runmilter("pythonfilter",socketname,timeout)
     logq.put(None)
     bt.join()
-    print("%s bms milter shutdown" % time.strftime('%Y%b%d %H:%M:%S'))
+    print "%s bms milter shutdown" % time.strftime('%Y%b%d %H:%M:%S')
 
 if __name__ == "__main__":
     main()
