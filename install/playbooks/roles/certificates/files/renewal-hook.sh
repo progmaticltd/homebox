ce90ea4a01d19882a501c72fd5a41ee024227bd7
André Rodier <andre@rodier.me>
2019-11-10 16:28:26 +0000

    An attept to reduce the certificate permissions
    The certificates should be readable only by their counterparts
    programs, for instance, imap / pop3 by dovecot, smtp by postfix,
    etc...
    
    This should fix #259

--- install/playbooks/roles/certificates/files/renewal-hook.sh
+++ install/playbooks/roles/certificates/files/renewal-hook.sh
@@ -16,10 +16,12 @@ for fqdn in $RENEWED_DOMAINS; do
         restart_nginx=$(grep -Rl "server_name $fqdn" /etc/nginx/sites-enabled/ | grep -v -e -cert -c)
     fi
 
+    # Make sure the full chain is readable by all programs
+    chmod a+r /etc/letsencrypt/archive/$fqdn/fullchain*.pem
+
     if [ "$restart_nginx" = "1" ]; then
         echo "Reloading main web site"
         systemctl restart nginx
         break
     fi
 done
-
