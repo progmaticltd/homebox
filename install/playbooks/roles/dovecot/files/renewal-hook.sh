43e463c8a4afa21bcd16b78f95d49bb4e80cc8f6
André Rodier <andre@rodier.me>
2019-12-14 12:28:22 +0000

    Removed www-data permissions to read certificates
    This is not necessary, nginx load the certificates as root.

---
+++ install/playbooks/roles/dovecot/files/renewal-hook.sh
@@ -0,0 +1,22 @@
+#!/bin/dash
+
+# This hook to allows dovecot to read new LDAP certificates
+
+. /etc/homebox/main.cf
+
+do_restart=0
+
+for fqdn in $RENEWED_DOMAINS; do
+
+    # Get the subdomain
+    sub=$(expr "$fqdn" : '\([^.]*\)')
+
+    if [ "$sub" = "ldap" ]; then
+        cd "$RENEWED_LINEAGE"
+        setfacl -n -m u:dovecot:r cert*pem *chain*.pem
+        # Restart the dovecot server (might be not needed)
+        echo "Reloading Dovecot server"
+        systemctl restart dovecot
+        break
+    fi
+done
