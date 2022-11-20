0fec27b073d2cb3895ec971de03b7bd4d208aa91
Andre Rodier <andre@montessori.rodier.me>
2022-11-20 15:47:20 +0000

    Cleanup and multiple changes

--- roles/roundcube/templates/plugins/dovecot_impersonate.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Set the dovecot seperator character.
-// Doc:  http://wiki.dovecot.org/Authentication/MasterUsers
-
-$rcmail_config['dovecot_impersonate_seperator'] = '{{ mail.impersonate.separator }}';
