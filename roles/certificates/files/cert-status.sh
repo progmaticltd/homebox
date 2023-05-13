6e8fd9896d5b84fa80814018c029c7b738747028
Andre Rodier <andre@montessori.rodier.me>
2023-05-13 11:23:50 +0100

    Multiple updates on certificates

--- roles/certificates/files/cert-status.sh
+++ roles/certificates/files/cert-status.sh
@@ -6,20 +6,39 @@ cd /etc/lego/certificates
 # List all certificate files by time
 cert_files=$(ls -1tr *.crt | grep -v issuer)
 
+# Create the temporary output
+temp_file=$(mktemp)
+
+# Remove temporary files on exit
+trap cleanup 1 2 3 6
+cleanup()
+{
+    rm -rf "temp_file"
+}
+
+# Create the summary table
 for cert_file in $cert_files; do
 
+    cafile=$(echo $cert_file | sed 's/^\.crt$/.issuer.crt/')
     issuer=$(openssl x509 -in $cert_file -noout -issuer | sed 's/.*= //')
     fqdn=$(openssl x509 -in $cert_file -noout -subject | sed -E 's/.* = (DNS:)?([\*a-z]+).*/\2/')
     from=$(openssl x509 -in $cert_file -noout -dates | sed -En 's/notBefore=(.*)/\1/p')
     till=$(openssl x509 -in $cert_file -noout -dates | sed -En 's/notAfter=(.*)/\1/p')
     sans=$(openssl x509 -in $cert_file -noout -ext subjectAltName | tail -n +2 | sed 's/ //g' | tr '\n' ',' | sed 's/,$//')
 
-    line=$(printf "%-16s: %-30s %-30s %-30s %s" "$fqdn" "$from" "$till" "$issuer" "$sans")
-
-    if openssl verify $cert_file >/dev/null 2>&1; then
-        echo "$line"
+    if openssl verify -untrusted $cafile $cert_file >/dev/null 2>&1; then
+        printf "%s|%s|%s|%s|%s|OK\n" "$fqdn" "$from" "$till" "$issuer" "$sans" >>$temp_file
     else
-        echo "$line" | colorize Red
+        error=$(openssl verify -CAfile $cafile $cert_file 2>&1 | sed -En 's/.*: //p')
+        error=$(echo "$error" | tr '\n' ',' | sed 's/,$//' | sed 's/,/, /g')
+        printf "%s|%s|%s|%s|%s|$error\n" "$fqdn" "$from" "$till" "$issuer" "$sans" >>$temp_file
     fi
 
 done
+
+# Display the output table formatted
+columns='Domain,Valid from,Valid until,Issuer,Full domains list,Status'
+column -t -s '|' -o '  | ' -N "$columns" -W Status $temp_file
+
+# Remove temporary files
+cleanup
