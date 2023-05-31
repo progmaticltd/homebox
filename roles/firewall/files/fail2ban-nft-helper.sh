defb21bf4d8c084f836814171ea32022a416b31e
Andre Rodier <andre@montessori.rodier.me>
2023-05-31 19:38:35 +0100

    Better fail2ban helper script

--- roles/firewall/files/fail2ban-nft-helper.sh
+++ roles/firewall/files/fail2ban-nft-helper.sh
@@ -1,82 +1,229 @@
 #!/bin/sh
 #
-## Work in progress
+## usage:
+##   fail2ban-nft-helper <action> [-v] -j <name> [-i ip]
+##
+## examples:
+##   fail2ban-nft-helper start -j postfix
+##   start the postfix jail
+##
+##   fail2ban-nft-helper stop -j postfix
+##   stop the postfix jail
+##
+##   fail2ban-nft-helper ban -j postfix -i 185.53.12.41
+##   ban the IP address 185.53.12.41 in the postfix jail
+#
 
-action=$1
+# status and error codes
+EXIT_SUCCESS=0
+EXIT_ARG_ERROR=10
+EXIT_IP_ERROR=20
+EXIT_TABLE_ERROR=30
 
-if [ "$action" = "start" ]; then
+usage() {
+    sed -n 's/^## //p' "$0"
+}
 
-    name=$2
+if [ -z "$1" ]; then
+    usage
+    exit $EXIT_ARG_ERROR
+fi
 
-    # Create the fail2ban filter if not existing,
-    # with a priority of -10 to run just before fitlers
-    nft 'add chain inet filter fail2ban { type filter hook input priority -10 ; }'
+# First, read the action parameter
+action=$(echo "$1" | sed -En 's/^(start|stop|ban|unban|check)$/\1/p')
+
+if [ -z "$action" ]; then
+    usage
+    exit $EXIT_ARG_ERROR
+else
+    shift
+fi
+
+# Default values when not specified
+name=""
+ip=""
+verbose="0"
+
+while getopts 'j:i:v' opt; do
+    case "$opt" in
+        (j)
+            # The jail name is always required
+            name=$(echo "$OPTARG" | sed 's/[^a-z-]*//g')
+            test -z "$name" && exit $EXIT_ARG_ERROR
+            ;;
+        (i)
+            ipcalc-ng -s -c "$OPTARG" || exit $EXIT_ARG_ERROR
+            ip="$OPTARG"
+            ;;
+        (v)
+            verbose=1
+            shift
+            ;;
+        (*)
+            echo "Invalid argument '$opt'"
+            exit $EXIT_ARG_ERROR
+        ;;
+    esac
+done
+
+
+if [ -z "$name" ]; then
+    echo "Please, specify the jail name"
+    exit $EXIT_ARG_ERROR
+fi
+
+# Display messages or not, depending the verbose flag
+if [ "$verbose" = "0" ]; then
+    message() {
+        return
+    }
+else
+    message() {
+        printf "%s\n" "$*" >&2;
+    }
+fi
+
+
+
+
+# Create the fail2ban filter if not existing,
+# with a priority of -10 to run just before filters
+start_jail() {
+
+    name="$1"
+
+    message "Starting jail '$name'"
+
+    nft "add chain inet filter fail2ban { type filter hook input priority -10 ; }"
 
     nft add set inet filter "f2b-${name}-ipv4" '{ type ipv4_addr ; }'
     nft add set inet filter "f2b-${name}-ipv6" '{ type ipv6_addr ; }'
 
     nft inet filter fail2ban ip saddr "@f2b-${name}-ipv4" counter reject
     nft inet filter fail2ban ip6 saddr "@f2b-${name}-ipv6" counter reject
+}
 
-    exit
-fi
+# Remove references to a jail, then the jail itself
+stop_jail() {
 
-if [ "$action" = "stop" ]; then
+    name="$1"
 
-    name=$2
-    port=$3
+    message "Stopping jail '$name'"
 
     ipv4_handle=$(nft -a list ruleset | sed -En "s/.*@f2b-${name}-ipv4.*handle ([0-9]+)/\\1/p")
     ipv6_handle=$(nft -a list ruleset | sed -En "s/.*@f2b-${name}-ipv6.*handle ([0-9]+)/\\1/p")
 
     if [ "$ipv4_handle" != "" ]; then
         nft delete rule inet filter fail2ban handle "$ipv4_handle"
-    elif [ "$ipv6_handle" != "" ]; then
+    else
+        message "$0: rule handle not found for '$name'."
+    fi
+
+    if [ "$ipv6_handle" != "" ]; then
         nft delete rule inet filter fail2ban handle "$ipv6_handle"
     else
-        echo "$0: rule handle not found for '$name'."
+        message "$0: rule handle not found for '$name'."
     fi
 
-    exit
-fi
-
-if [ "$action" = "check" ]; then
-
-    name=$2
-    port=$3
-
-    nft list set inet filter "f2b-$name-ipv4"
-    nft list set inet filter "f2b-$name-ipv6"
-
-    exit
-fi
-
-if [ "$action" = "ban" ]; then
-
-    name=$2
-    ip=$3
-    type=$4
-
-    if echo "$ip" | grep -P '^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$'; then
-        nft add element inet filter "f2b-$name-ipv4" "{ $ip }"
+    # Then, delete the sets if they exists
+    if ! nft list set inet filter "f2b-${name}-ipv4" >/dev/null; then
+        message "Warning: set 'f2b-${name}-ipv4' doesn't exists, ignoring."
     else
-        nft add element inet filter "f2b-$name-ipv6" "{ $ip }"
+        nft delete set inet filter "f2b-${name}-ipv4";
     fi
 
-    exit
-fi
-
-if [ "$action" = "unban" ]; then
-
-    name=$2
-    ip=$3
-    type=$4
-
-    if echo "$ip" | grep -P '^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$'; then
-        nft delete element inet filter "f2b-$name-ipv4" "{ $ip }"
+    if ! nft list set inet filter "f2b-${name}-ipv6" >/dev/null; then
+        message "Warning: set 'f2b-${name}-ipv6' doesn't exists, ignoring."
     else
-        nft delete element inet filter "f2b-$name-ipv6" "{ $ip }"
+        nft delete set inet filter "f2b-${name}-ipv6";
+    fi
+}
+
+check_jail() {
+
+    name="$1"
+
+    message "Checking jail '$name'"
+
+    if nft list set inet filter "f2b-${name}-ipv4" >/dev/null 2>&1; then
+        message " - IPv4 OK"
+    else
+        message " - IPv4 not found"
     fi
 
-    exit
-fi
+    if nft list set inet filter "f2b-${name}-ipv6" >/dev/null 2>&1; then
+        message " - IPv6 OK"
+    else
+        message " - IPv6 not found"
+    fi
+
+}
+
+
+ban() {
+
+    name="$1"
+    ip="$2"
+    type=""
+
+    if ipcalc-ng -4 -c "$ip"; then
+        type="ipv4"
+    elif ipcalc-ng -6 -c "$ip"; then
+        type="ipv6"
+    else
+        message "IP address is not valid"
+        exit $EXIT_IP_ERROR
+    fi
+
+    if nft add element inet filter "f2b-${name}-${type}" "{ $ip }"; then
+        message "Banning ${type} '$ip' for jail '${name}': Success"
+    else
+        message "Banning ${type} '$ip' for jail '${name}': Error"
+    fi
+
+}
+
+unban() {
+
+    name="$1"
+    ip="$2"
+    type=""
+
+    if ipcalc-ng -4 -c "$ip"; then
+        type="ipv4"
+    elif ipcalc-ng -6 -c "$ip"; then
+        type="ipv6"
+    else
+        message "IP address is not valid"
+        exit $EXIT_IP_ERROR
+    fi
+
+    if nft delete element inet filter "f2b-${name}-${type}" "{ $ip }"; then
+        message "Unbanning ${type} '$ip' for jail '${name}': Success"
+    else
+        message "Unbanning ${type} '$ip' for jail '${name}': Error"
+    fi
+
+}
+
+case "$action" in
+    start)
+        start_jail "$name"
+        ;;
+    stop)
+        stop_jail "$name"
+        ;;
+    ban)
+        ipcalc-ng -s -c "$ip" || exit $EXIT_ARG_ERROR
+        ban "$name" "$ip"
+        ;;
+    unban)
+        ipcalc-ng -s -c "$ip" || exit $EXIT_ARG_ERROR
+        unban "$name" "$ip"
+        ;;
+    check)
+        check_jail "$name"
+        ;;
+    *)
+        exit $EXIT_ARG_ERROR
+esac
