844e0ad84b963e5b574a2f01f54b3e9157c1acb0
Andre Rodier <andre@montessori.rodier.me>
2023-06-16 20:39:05 +0100

    Multiple updates

---
+++ roles/dovecot/files/antispam/noop.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# To activate the antispam learning feature, replace this script
+# using an antispam third party package, for instance, rspamd.
+
+exit 0
--- roles/rspamd/files/dovecot/learn-hamorspam.sh
+++ roles/rspamd/files/dovecot/learn-hamorspam.sh
@@ -6,18 +6,24 @@ type=$1
 # Error codes
 success=0
 param_error=10
-runtime_error=20
+socket_error=20
+runtime_error=30
+
+# rspamd socket to use
+socket="/run/rspamd/controller.sock"
 
 if [ "$type" != "spam" ] && [ "$type" != "ham" ]; then
     logger "Invalid type parameter for ham or spam: '$type'"
     exit $param_error
 fi
 
-# rspamd socket to use
-host="unix:/run/rspamd/controller.sock"
+if [ ! -w "$socket" ]; then
+    echo "Cannot open socket file for writing"
+    exit $runtime_error
+fi
 
-if ! /usr/bin/rspamc -v -h "$host" "learn_${type}"; then
-    logger "Error when running rspamc client"
+if ! /usr/bin/rspamc -v -h "$socket" "learn_${type}"; then
+    echo "Error when running rspamc client"
     exit $runtime_error
 fi
 
