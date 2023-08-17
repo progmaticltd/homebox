f23b373a19dfd4ddb5ea007e2b089eb843f15feb
Andre Rodier <andre@lovelace.rodier.me>
2023-08-17 12:57:01 +0100

    wip: implemented borg-backup uninstall role

--- roles/borg-backup/templates/cron-backup-script.sh
+++ roles/borg-backup/templates/cron-backup-script.sh
@@ -1,5 +1,13 @@
 #!/bin/dash
 
+# Ensure the drive is mounted
+ls /mnt/backup/{{ location.name }}
+
+if ! mountpoint -q /mnt/backup/{{ location.name }}; then
+    echo "The backup location {{ location.name }} is not mounted"
+    exit 1
+fi
+
 # Call the global script helper
 {% if system.debug %}
 /usr/local/sbin/homebox-backup --config '{{ location.name }}' --log-level DEBUG
