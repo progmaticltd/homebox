c7f024604fd9332984899b52cbfd3633bafe9692
Andre Rodier <andre@lovelace.rodier.me>
2024-02-07 07:23:45 +0000

    feat: Regenerate dane records on certificate reissueing

--- roles/postfix/files/renew-cert.sh
+++ roles/postfix/files/renew-cert.sh
@@ -1,9 +1,9 @@
 #!/bin/dash
 
+# Refresh the DANE record if needed
+/usr/local/sbin/dane-set-record smtp 25
+
 # Restart postfix at midnight
-
 if ! cat /var/spool/cron/atjobs/* | grep -c 'systemctl restart postfix'; then
-
     echo 'systemctl restart postfix' | at midnight
-
 fi
